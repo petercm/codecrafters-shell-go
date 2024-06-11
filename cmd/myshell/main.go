1f9367779e54d8c17c51d6af65c0ba6675f63da5
Peter Miller <petercm@gmail.com>
2024-06-11 17:30:22 -0400

    path support for type builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"slices"
@@ -11,6 +12,18 @@ import (
 
 func getBuiltins() []string {
 	return []string{"echo", "exit", "type"}
+}
+
+func pathCmd(inputTokens []string) (string, bool) {
+	cmd := inputTokens[0]
+	paths := strings.Split(os.Getenv("PATH"), ":")
+	for _, path := range paths {
+		cmdPath := fmt.Sprintf("%s/%s", path, cmd)
+		if _, err := os.Stat(cmdPath); !errors.Is(err, os.ErrNotExist) {
+			return cmdPath, true
+		}
+	}
+	return "", false
 }
 
 func execCmd(inputTokens []string) {
@@ -29,6 +42,8 @@ func execCmd(inputTokens []string) {
 		typeOption := inputTokens[1]
 		if slices.Contains(getBuiltins(), typeOption) {
 			fmt.Printf("%s is a shell builtin\n", typeOption)
+		} else if cmdPath, found := pathCmd(inputTokens[1:]); found {
+			fmt.Printf("%s is %s\n", typeOption, cmdPath)
 		} else {
 			fmt.Printf("%s: not found\n", typeOption)
 		}
