5036f95eab2a6d120c129cce852983d1771a4118
Peter Miller <petercm@gmail.com>
2024-06-11 15:40:34 -0400

    Echo cmd

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,6 +8,25 @@ import (
 	"strings"
 )
 
+func execCmd(inputTokens []string) {
+	switch inputTokens[0] {
+	case "echo":
+		fmt.Println(strings.Join(inputTokens[1:], " "))
+	case "exit":
+		exitCode := 0
+		if len(inputTokens) > 1 {
+			exitCode, _ = strconv.Atoi(inputTokens[1])
+			// if err != nil {
+			// 	exitCode = 0
+			// }
+		}
+
+		os.Exit(exitCode)
+	default:
+		fmt.Printf("%s: command not found\n", inputTokens[0])
+	}
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	// Wait for user input
@@ -19,16 +38,7 @@ func main() {
 		}
 		input = strings.TrimSuffix(input, "\n")
 		inputTokens := strings.Split(input, " ")
-		switch inputTokens[0] {
-		case "exit":
-			exitCode, err := strconv.Atoi(inputTokens[1])
-			if err != nil {
-				exitCode = 0
-			}
-			os.Exit(exitCode)
-		default:
-			fmt.Printf("%s: command not found\n", input)
-		}
+		execCmd(inputTokens)
 	}
 
 }
