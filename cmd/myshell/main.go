af225ce7cde097f045171c96818c2ca426d90e79
Peter Miller <petercm@gmail.com>
2024-06-11 21:55:32 -0400

    run external programs

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"slices"
 	"strconv"
 	"strings"
@@ -12,18 +12,6 @@ import (
 
 func getBuiltins() []string {
 	return []string{"echo", "exit", "type"}
-}
-
-func pathCmd(inputTokens []string) (string, bool) {
-	cmd := inputTokens[0]
-	paths := strings.Split(os.Getenv("PATH"), ":")
-	for _, path := range paths {
-		cmdPath := fmt.Sprintf("%s/%s", path, cmd)
-		if _, err := os.Stat(cmdPath); !errors.Is(err, os.ErrNotExist) {
-			return cmdPath, true
-		}
-	}
-	return "", false
 }
 
 func execCmd(inputTokens []string) {
@@ -42,13 +30,19 @@ func execCmd(inputTokens []string) {
 		typeOption := inputTokens[1]
 		if slices.Contains(getBuiltins(), typeOption) {
 			fmt.Printf("%s is a shell builtin\n", typeOption)
-		} else if cmdPath, found := pathCmd(inputTokens[1:]); found {
+		} else if cmdPath, err := exec.LookPath(inputTokens[1]); err == nil {
 			fmt.Printf("%s is %s\n", typeOption, cmdPath)
 		} else {
 			fmt.Printf("%s: not found\n", typeOption)
 		}
 	default:
-		fmt.Printf("%s: command not found\n", cmd)
+		path, err := exec.LookPath(cmd)
+		if err != nil {
+			fmt.Printf("%s: command not found\n", cmd)
+		} else {
+			out, _ := exec.Command(path, inputTokens[1:]...).CombinedOutput()
+			fmt.Printf("%s", out)
+		}
 	}
 }
 
