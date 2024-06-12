5648f043bf80797d1546181db1b5cde402221a63
Peter Miller <petercm@gmail.com>
2024-06-11 22:18:49 -0400

    cd builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,26 +11,27 @@ import (
 )
 
 func getBuiltins() []string {
-	return []string{"echo", "exit", "type", "pwd"}
+	return []string{"echo", "exit", "type", "pwd", "cd"}
 }
 
 func execCmd(inputTokens []string) {
 	cmd := inputTokens[0]
+	arguments := inputTokens[1:]
 	switch cmd {
 	case "echo":
-		fmt.Println(strings.Join(inputTokens[1:], " "))
+		fmt.Println(strings.Join(arguments, " "))
 	case "exit":
 		exitCode := 0
-		if len(inputTokens) > 1 {
-			exitCode, _ = strconv.Atoi(inputTokens[1])
+		if len(arguments) > 0 {
+			exitCode, _ = strconv.Atoi(arguments[0])
 		}
 
 		os.Exit(exitCode)
 	case "type":
-		typeOption := inputTokens[1]
+		typeOption := arguments[0]
 		if slices.Contains(getBuiltins(), typeOption) {
 			fmt.Printf("%s is a shell builtin\n", typeOption)
-		} else if cmdPath, err := exec.LookPath(inputTokens[1]); err == nil {
+		} else if cmdPath, err := exec.LookPath(arguments[0]); err == nil {
 			fmt.Printf("%s is %s\n", typeOption, cmdPath)
 		} else {
 			fmt.Printf("%s: not found\n", typeOption)
@@ -38,12 +39,17 @@ func execCmd(inputTokens []string) {
 	case "pwd":
 		dir, _ := os.Getwd()
 		fmt.Println(dir)
+	case "cd":
+		err := os.Chdir(arguments[0])
+		if err != nil {
+			fmt.Printf("cd: %s: No such file or directory\n", arguments[0])
+		}
 	default:
 		path, err := exec.LookPath(cmd)
 		if err != nil {
 			fmt.Printf("%s: command not found\n", cmd)
 		} else {
-			out, _ := exec.Command(path, inputTokens[1:]...).CombinedOutput()
+			out, _ := exec.Command(path, arguments...).CombinedOutput()
 			fmt.Printf("%s", out)
 		}
 	}
